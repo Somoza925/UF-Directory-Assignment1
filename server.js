4e067d769eb5187dcd53022691a4b1ec339c5d27
Somoza <Somoza0315@gmail.com>
2018-09-17 17:05:40 -0400

    completed assigment

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+
+var parsedUrl = url.parse(request.url);
+var data = JSON.stringify(listingData);
+if (request.method == 'GET' && parsedUrl.pathname == '/listings'){
+    console.log("hi")
+    response.writeHead(200, {
+      'Content-Type': 'text/plain' });
+    
+    response.end(data);
+} else {
+  response.writeHead(404, {
+    'Content-Type': 'text/plain' });
+  response.end("Bad gateway error");
+}
+
 
   /*
     Your request handler should send listingData in the JSON format if a GET request 
@@ -18,9 +32,25 @@ var requestHandler = function(request, response) {
    */
 };
 
+var server = http.createServer(requestHandler);
+
 fs.readFile('listings.json', 'utf8', function(err, data) {
+  if (err){
+    console.log(err);
+  }
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+   listingData = JSON.parse(data);
+
+   // a server is created, but not started
+   var server = http.createServer(requestHandler);
+
+   server.listen(port, function() {
+   //once the server is listening, this callback function is executed
+   console.log('Server listening on: http://127.0.0.1:' + port);
+    });
 });
+
+
